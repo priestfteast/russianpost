1da0dc14dde65ca1025e5cd9e7f828c1ae2dabe3
priestfteast <htcp3450@mail.ru>
2020-09-28 11:29:16 +0300

    Initial commit

---
+++ src/main/java/ru/comearth/russianpost/RussianpostApplication.java
@@ -0,0 +1,13 @@
+package ru.comearth.russianpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RussianpostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RussianpostApplication.class, args);
+    }
+
+}
