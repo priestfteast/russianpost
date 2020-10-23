c78df380c9df107f438db80ee2a515edb97d0e30
priestfteast <39798423+priestfteast@users.noreply.github.com>
2020-10-23 11:17:05 +0300

    Update RussianpostApplication.java

--- src/main/java/ru/comearth/russianpost/RussianpostApplication.java
+++ src/main/java/ru/comearth/russianpost/RussianpostApplication.java
@@ -11,7 +11,7 @@ public class RussianpostApplication {
     public static void main(String[] args) {
         ApiContextInitializer.init();
         SpringApplication.run(RussianpostApplication.class, args);
-        Bot test_habr_bot = new Bot("ComSystems_Bot", "1363750766:AAFnPA5030JA0Eco7VVLOEeJ7VnhHE-WVPc");
+        Bot test_habr_bot = new Bot("Co", "c");
         test_habr_bot.botConnect();
     }
 
